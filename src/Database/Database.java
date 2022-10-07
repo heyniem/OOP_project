4114af720f63ecca9cf0b041465fd7a8953231be
quanandon <tranngocanhquan03@gmail.com>
2022-10-07 11:49:15 +0700

    Fix Bomberman movement to make it easier to move and add Balloon enermy. Next thing: Do the collision between Entities(Mons vs Mons, Mons vs Bomberman)

---
+++ src/Database/Database.java
@@ -0,0 +1,24 @@
+package Database;
+
+import Input.KeyHandle;
+import entities.AnimatedEntities.Characters.Bomberman;
+import entities.Entity;
+import graphics.Sprite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    public static final int WIDTH = 20;
+    public static final int HEIGHT = 15;
+
+    public static Entity bomber = new Bomberman(1, 1, Sprite.doll_left1.getFxImage());
+
+
+    public static List<Entity> entities = new ArrayList<>();
+    public static List<Entity> stillObjects = new ArrayList<>();
+
+    public static KeyHandle keyHandle = new KeyHandle();
+
+
+}
